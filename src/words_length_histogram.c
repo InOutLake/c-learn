8dc66e0b3e397efecc7ebeaf545c75ffeaea7b07
inoutlake <evpatcolovratez@gmail.com>
2025-06-25 00:39:37 +0700

    gistogram +- ready

--- src/words_length_histogram.c
+++ src/words_length_histogram.c
@@ -1,46 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *print_column(int value, int width_outside, int width_inside) {
+char *format_column(int value, int width_outside, int width_inside) {
   const char *fill_char = " ";
   const char *blocks[] = {
-      "\xE2\x96\x81", // ▁  One eighth block       (index 0)
-      "\xE2\x96\x82", // ▂  One quarter block      (index 1)
-      "\xE2\x96\x83", // ▃  Three eighths block    (index 2)
-      "\xE2\x96\x84", // ▄  Half block             (index 3)
-      "\xE2\x96\x85", // ▅  Five eighths block     (index 4)
-      "\xE2\x96\x86", // ▆  Three quarters block   (index 5)
-      "\xE2\x96\x87", // ▇  Seven eighths block    (index 6)
-      "\xE2\x96\x88", // █  Full block             (index 7)
-      "\xE2\x96\x88", // █  Full block (extra)     (index 8)
-      "\xE2\x96\x88"  // █  Full block (extra)     (index 9)
+      " ",
+      "\xE2\x96\x81", // ▁  One eighth block
+      "\xE2\x96\x82", // ▂  One quarter block
+      "\xE2\x96\x83", // ▃  Three eighths bloc
+      "\xE2\x96\x84", // ▄  Half block
+      "\xE2\x96\x85", // ▅  Five eighths block
+      "\xE2\x96\x86", // ▆  Three quarters block
+      "\xE2\x96\x87", // ▇  Seven eighths block
+      "\xE2\x96\x88", // █  Full block
+      "\xE2\x96\x88", // █  Full block
   };
   int padding = width_outside - width_inside;
   int pad_left = padding / 2;
   int pad_right = padding - pad_left;
-  char *buffer = (char *)malloc(width_outside * 2);
-  char *result = buffer;
+  int buffer_size = (width_outside * 4);
+  char *buffer = calloc(buffer_size, sizeof(char));
+  char *ptr = buffer;
+
   for (int i = 0; i < pad_left; i++) {
-    result += sprintf(result, "%s\n", fill_char);
+    ptr += sprintf(ptr, "%s", fill_char);
   }
+
   for (int i = 0; i < width_inside; i++) {
-    result += sprintf(result, "%s\n", blocks[value]);
+    ptr += sprintf(ptr, "%s", blocks[value]);
   }
+
   for (int i = 0; i < pad_right; i++) {
-    result += sprintf(result, "%s\n", fill_char);
+    ptr += sprintf(ptr, "%s", fill_char);
   }
-  return result;
+
+  return buffer;
 };
 
-#define COLUMN_WIDTH_OUTER 4
-#define COLUMNT_WIDTH_INSIDE 2
-#define MAX_HEIGHT 200
-#define MAX_WORD_LENGTH 15
+#define COLUMN_WIDTH_OUTER 3
+#define COLUMN_WIDTH_INSIDE 2
+#define MAX_HEIGHT 100
+#define MAX_WORD_LENGTH 11
 int main(int argc, char *argv[]) {
   int lengths[MAX_WORD_LENGTH + 1];
+  for (int i = 0; i < MAX_WORD_LENGTH; i++) {
+    lengths[i] = 0;
+  }
   int word_length = 0;
   int ch;
-  FILE *file = fopen("/home/inoutlake/c-learn/tmp/histogram.txt", "r");
+  FILE *file = fopen("/home/inoutlake/projects/c-learn/tmp/histogram.txt", "r");
+  if (file == NULL) {
+    perror("Error opening file");
+    return EXIT_FAILURE;
+  }
   while ((ch = fgetc(file)) != EOF) {
     if (ch == ' ' || ch == '\t' || ch == '\n') {
       if (word_length > MAX_WORD_LENGTH)
@@ -52,9 +64,20 @@ int main(int argc, char *argv[]) {
     }
   }
   for (int h = MAX_HEIGHT; h > 0; h -= 10) {
-    for (int col = 1; col < MAX_WORD_LENGTH; h++) {
-      print_column()
+    for (int col = 1; col < MAX_WORD_LENGTH; col++) {
+      int value = (lengths[col] - h);
+      if (value < 0) {
+        value = 0;
+      } else if (value > 10) {
+        value = 9;
+      }
+      printf("%s",
+             format_column(value, COLUMN_WIDTH_OUTER, COLUMN_WIDTH_INSIDE));
     }
+    printf("%s\n", "");
+  }
+  for (int col = 1; col < MAX_WORD_LENGTH; col++) {
+    printf("%3d", col);
   }
   return EXIT_SUCCESS;
 }
